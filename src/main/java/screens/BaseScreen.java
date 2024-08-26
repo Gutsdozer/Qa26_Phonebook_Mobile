d1fcf170d8c258851bae343fe876d0f1c13c74e2
Gutsdozer <warderbass@yandex.ru>
2024-08-26 20:52:44 +0300

    666

---
+++ src/main/java/screens/BaseScreen.java
@@ -0,0 +1,25 @@
+package screens;
+
+import config.AppiumConfig;
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.android.AndroidElement;
+import io.appium.java_client.pagefactory.AppiumFieldDecorator;
+import org.openqa.selenium.support.PageFactory;
+
+public class BaseScreen {
+
+    AppiumDriver<AndroidElement> driver;
+
+    public BaseScreen(AppiumDriver<AndroidElement> driver) {
+        this.driver = driver;
+        PageFactory.initElements(new AppiumFieldDecorator(driver), this);
+    }
+
+    public void type(AndroidElement element, String text){
+        if(text!=null){
+            element.click();
+            element.clear();
+            element.sendKeys(text);
+        }
+    }
+}
